c1379b2346f24138a540ad6f2bdd6d6cebec2653
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-11-12 23:16:18 +0300

    Image Reading OPENCV

---
+++ section01/read_image_video_webcam/read_image.py
@@ -0,0 +1,5 @@
+import cv2 
+import numpy as np
+image = cv2.imread('../images/image2.jpg')
+cv2.imshow('image', image)
+cv2.waitKey(0)
---
+++ section01/read_image_video_webcam/read_video.py
@@ -0,0 +1,2 @@
+import cv2
+cp=cv2.VideoCapture("../videos/bikes.mp4")
